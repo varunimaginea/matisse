674b84de2bd6d297d511faf6bad332db947a0575
manojiitkgp09 <manoj.a@imaginea.com>
2011-12-05 17:00:31 +0530

    Fixed broadcast issue and added redis connection

--- app.js
+++ app.js
@@ -3,23 +3,31 @@
  * Module dependencies.
  */
 
+var redis = require("redis")
+, redis_client = redis.createClient(); //go thru redis readme for anyother config other than default: localhost 6379
+
+redis_client.on("error", function (err) {
+    console.log("Error %s", err);
+});
+
+
 var express = require('express')
 , Resource = require('express-resource')
-  , routes = require('./routes')
+, routes = require('./routes')
 
 var app = module.exports = express.createServer()
-	, io = require('socket.io').listen(app);
+, io = require('socket.io').listen(app);
 	
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -49,63 +57,76 @@ app.get('/html', function (req, res) {
 });
 
 app.resource({
-  show: function(req, res){
-    res.sendfile(__dirname + '/index.html');
-  }
+    show: function(req, res){
+	res.sendfile(__dirname + '/index.html');
+    }
 });
 
 app.listen(8000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('eventConnect',{message:'welcome'});
-  socket.on('eventDraw',function(data){
-        socket.broadcast.emit("eventDraw",data);
-  });
+    socket.emit('eventConnect',{message:'welcome'});
+ 
 //    console.log("====================");
 //    console.log(socket);
 //    console.log("====================");
-    socket.emit('eventConnect',{message:'welcome'});
-    socket.on("setUrl",function(location,data){
-//     console.log("++++++++------------");
-//     console.log(data);
-//     console.log(location);
-//    console.log("++++++++-----------");
-       var url = location.replace("/", "");
-//     console.log("++++++++++++++++++++");
-//     console.log(url);
-//     console.log("++++++++++++++++++++");
-//     console.log("----------------------");
-//     console.log(data);
-//     console.log("----------------------");
+socket.emit('eventConnect',{message:'welcome'});
+socket.on("setUrl",function(location,data){
+   var url = location.replace("/", "");
+    ko = new Date();
+    ji = ko.getTime();
+//    socket.broadcast.emit("eventDraw",data);
+    redis_client.hset([url, socket.id+"#"+ji, data], redis.print);
 
-       socket.join(url);
+    //     console.log("++++++++------------");
+    //     console.log(data);
+    //     console.log(location);
+    //    console.log("++++++++-----------");
+    //     console.log("++++++++++++++++++++");
+    //     console.log(url);
+    //     console.log("++++++++++++++++++++");
+    //     console.log("----------------------");
+    //     console.log(data);
+    //     console.log("----------------------");
+    
+    socket.join(url);
+    redis_client.hkeys(url, function (err, replies) {
+	console.log("++++++++++++++++++++");
+        console.log(replies.length + " replies:");
+	console.log("++++++++++++++++++++");
+        replies.forEach(function (reply, i) {
+	    console.log("    " + i + ": " + reply);
+        });
+//	redis_client.quit();
     });
+});
 
-    socket.on('eventDraw',function(location,data){
-//     console.log("@@@@@@@@@@@@@@@@@@@@@");
-//     console.log(location);
-       var url = location.replace("/", "");
-//     console.log(url);
-//     console.log("@@@@@@@@@@@@@@@@@@@@@");
-//     console.log("%%%%%%%%%%%%%%%%%%%%%");
-//     console.log(data);
-//     console.log("%%%%%%%%%%%%%%%%%%%%%");
-	console.log("%%%%%%%%%%%%%%%%%%%%%");
-	console.log(io.sockets);
-	console.log("%%%%%%%%%%%%%%%%%%%%%");
-	console.log("@@@@@@@@@@@@@@@@@@@@@");
-	console.log(socket);
-	console.log("@@@@@@@@@@@@@@@@@@@@@");
-//	delete io.sockets(socket.id);
-	socket.broadcast.to(url).emit('eventDraw',data);
-  //     io.sockets.in(url).emit("eventDraw",data);
-    });
- });
- 
+socket.on('eventDraw',function(location,data){
+    //     console.log("@@@@@@@@@@@@@@@@@@@@@");
+    //     console.log(location);
+    var url = location.replace("/", "");
+    //     console.log(url);
+    //     console.log("@@@@@@@@@@@@@@@@@@@@@");
+    //     console.log("%%%%%%%%%%%%%%%%%%%%%");
+    //     console.log(data);
+    //     console.log("%%%%%%%%%%%%%%%%%%%%%");
+    //	console.log("%%%%%%%%%%%%%%%%%%%%%");
+    //	console.log(io.sockets);
+    //	console.log("%%%%%%%%%%%%%%%%%%%%%");
+    //	console.log("@@@@@@@@@@@@@@@@@@@@@");
+    //	console.log(socket);
+    //	console.log("@@@@@@@@@@@@@@@@@@@@@");
+    //	delete io.sockets(socket.id);
+    socket.broadcast.to(url).emit('eventDraw',data);
+    //io.sockets.in(url).emit("eventDraw",data);
+});
+});
+
+
 //io.sockets.on('connection', function (socket) {
 //  socket.emit('eventConnect',{message:'welcome'});
-  //socket.on('eventDraw',function(data){
+//socket.on('eventDraw',function(data){
 //      socket.join();
 //        socket.broadcast.emit("eventDraw",data);
 //  });
