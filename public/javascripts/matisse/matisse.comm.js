ceb01ae39d8b48803f22243be33a6c187095352f
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-01-20 16:19:40 +0530

    refactored , created class and prototype functions

--- public/javascripts/matisse/matisse.comm.js
+++ public/javascripts/matisse/matisse.comm.js
@@ -1,39 +1,39 @@
 /**
-* About this : Client side socket connection and communicaiton handler
+ * About this : Client side socket connection and communicaiton handler
  * 
  * 
 */
-define(["matisse", "matisse.util"],function(matisse, util) {
-	 var com = {
-		connectHandler: function(data){
+define(function () {
+    /*constructor*/
+	function Comm(url) {
+		this.socket = io.connect(url); 
+		var objRef =this;
+		this.socket.on("eventDraw", function (data) {
+			objRef.drawHandler(data);
+		});
+		this.socket.on('eventConnect', function (data) {
+			console.log("We are now connected");
+			objRef.connectHandler(data);	
+		});
+	}
+	(function(){
+		this.connectHandler = function (data) {
 			this.onConnect(data);
-		},
-		drawHandler: function(data){
+		};
+		this.drawHandler = function (data) {
 			this.onDraw(data);
-		},
-		sendDrawMsg: function(data){
+		};
+		this.sendDrawMsg = function (data) {
 			var loc = document.location.pathname;
-			this.socket.emit("eventDraw",loc,data);
-		},
-		onDraw: function(data){
+			this.socket.emit("eventDraw", loc, data);
+		};
+		this.onDraw = function (data) {
 			//Dummy method must override
-		},
-		onConnect: function(data){
+		};
+		this.onConnect =  function (data) {
 			var loc = document.location.pathname;
-			this.socket.emit("setUrl",loc,data);
-		},
-		init: function(){
-			var socket = io.connect('http://localhost'); //change it to server ip or local ip for testing from other machines
-			socket.on("eventDraw", function(data){
-				com.drawHandler(data);
-			});
-			socket.on('eventConnect', function (data) {
-				console.log("We are now connected");
-				com.connectHandler(data);	
-			});
-			this.socket = socket;
-		}
-	};
-	com.init();
-	return com;
+			this.socket.emit("setUrl", loc, data);
+		};
+	}).call(Comm.prototype);
+	return Comm;
 });
