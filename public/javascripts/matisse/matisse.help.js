b4dd906f8d452dcd2aef83c97c5e60409e42658e
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-01-23 15:41:19 +0530

    updated shortcut key text

--- public/javascripts/matisse/matisse.help.js
+++ public/javascripts/matisse/matisse.help.js
@@ -1,4 +1,4 @@
 function showHelp() {
-$("#result").html('<div id="helpInnerDiv"><h1>Help</h1><h2>Short Cuts:</h2><p><b>Move(1px) : </b>Arrows</p><p><b>Move(5px) : </b>Shift+Arrows</p><p><b>BringForward : </b>Alt+Up Arrow</p><p><b>SendBackwards : </b>Alt+Down Arrow</p><p><b>Delete : </b>Alt+Delete</p><p><Fig01 - Below diagrams describes about each icon in this app</p><img src="/images/help/help_00.png" /></div>');
+$("#result").html('<div id="helpInnerDiv"><h1>Help</h1><h2>Short Cuts:</h2><p><b>Move(1px) : </b>Arrows</p><p><b>Move(5px) : </b>Shift+Arrows</p><p><b>BringForward : </b>Ctrl+Up Arrow</p><p><b>SendBackwards : </b>Ctrl+Down Arrow</p><p><b>Delete : </b>Alt+Delete</p><p><Fig01 - Below diagrams describes about each icon in this app</p><img src="/images/help/help_00.png" /></div>');
 popup('popUpDiv', 'closediv', 800, 600);
 }
