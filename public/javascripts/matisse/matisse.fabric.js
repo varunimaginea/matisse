f4a9254a3ba3ff1dd1d9fe0d9aebc261b13e59db
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-01-23 12:05:26 +0530

    Correcting indentation and other code formatting issues

--- public/javascripts/matisse/matisse.fabric.js
+++ public/javascripts/matisse/matisse.fabric.js
@@ -1,6 +1,6 @@
 /* fabric related methods */
 define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", "matisse.events"], function (matisse, util, properties, ui, events) {
-
+	'use strict';
     /**
      *  Check for the event fired by fabric when any of the canvas objects modified and apply update properites panel accordingly
      *  @method  observe
@@ -24,7 +24,7 @@ define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", 
                             uid: obj.uid,
                             object: obj
                         }] // When sent only 'object' for some reason object  'uid' is not available to the receiver method.
-                    })
+                    });
                     properties.updatePropertyPanel(obj);
                     break;
                 case "selection:cleared":
@@ -37,21 +37,21 @@ define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", 
                     ui.resetIconSelection();
                     matisse.drawShape = false;
                     document.getElementById("c").style.cursor = 'default';
-                    var obj = e.memo.path;
-                    obj.uid = util.uniqid();
-                    obj.name = "drawingpath";
-                    obj.palette = matisse.paletteName;
+                    var pathObj = e.memo.path;
+                    pathObj.uid = util.uniqid();
+                    pathObj.name = "drawingpath";
+                    pathObj.palette = matisse.paletteName;
                     matisse.comm.sendDrawMsg({
                         action: 'drawpath',
                         palette: matisse.paletteName,
                         args: [{
-                            uid: obj.uid,
-                            left: obj.left,
-                            top: obj.top,
-                            width: obj.width,
-                            height: obj.height,
-                            path: obj.path,
-                            name: obj.name,
+                            uid: pathObj.uid,
+                            left: pathObj.left,
+                            top: pathObj.top,
+                            width: pathObj.width,
+                            height: pathObj.height,
+                            path: pathObj.path,
+                            name: pathObj.name,
                             palette: matisse.paletteName
                         }]
                     });
@@ -59,44 +59,39 @@ define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", 
                     matisse.yPoints = [];
                     break;
                 case 'object:selected':
-                    var obj = e.memo.target;
+                    var selectedObj = e.memo.target;
                     if (canvas.getActiveGroup()) {
                         return;
                     }
-                    properties.createPropertiesPanel(obj);
+                    properties.createPropertiesPanel(selectedObj);
                     break;
                 case 'object:moving':
-                    var obj = e.memo.target;
-                    checkAlign(obj);
+                    var movingObj = e.memo.target;
+                    checkAlign(movingObj);
                     break;
                 }
-            })
-
+            });
         }
-
-
-
-
-
-    }
+    };
     /* Get all objects from canvas and check if borders of any of them matches with active object borders
      */
     function checkAlign(obj) {
-        var hLine = matisse.hLine;
-        var vLine = matisse.vLine;
+        var hLine = matisse.hLine,
+			vLine = matisse.vLine;
         hLine.set('top', -10);
         vLine.set('left', -10);
-        var movingObjLeft = Math.round(obj.left - (obj.width * obj.scaleX) / 2);
-        var movingObjTop = Math.round(obj.top - (obj.height * obj.scaleY) / 2);
-        var movingObjRight = Math.round(obj.left + (obj.width * obj.scaleX) / 2);
-        var movingObjBottom = Math.round(obj.top + (obj.height * obj.scaleY) / 2);
-        var canvasObjects = canvas.getObjects();
-        for (var i = 0; i < canvasObjects.length; i++) {
-            var otherObjLeft = Math.round(canvasObjects[i].left - (canvasObjects[i].width * canvasObjects[i].scaleX) / 2);
-            var otherObjTop = Math.round(canvasObjects[i].top - (canvasObjects[i].height * canvasObjects[i].scaleY) / 2);
-            var otherObjRight = Math.round(canvasObjects[i].left + (canvasObjects[i].width * canvasObjects[i].scaleX) / 2);
-            var otherObjBottom = Math.round(canvasObjects[i].top + (canvasObjects[i].height * canvasObjects[i].scaleY) / 2);
-            if (canvasObjects[i] != obj && canvasObjects[i].name != 'vline' && canvasObjects[i].name != 'hline') { /* this LEFT matches with Other LEFT */
+        var movingObjLeft = Math.round(obj.left - (obj.width * obj.scaleX) / 2),
+			movingObjTop = Math.round(obj.top - (obj.height * obj.scaleY) / 2),
+			movingObjRight = Math.round(obj.left + (obj.width * obj.scaleX) / 2),
+			movingObjBottom = Math.round(obj.top + (obj.height * obj.scaleY) / 2),
+			canvasObjects = canvas.getObjects(),
+			i = 0;
+        for (i; i < canvasObjects.length; i++) {
+            var otherObjLeft = Math.round(canvasObjects[i].left - (canvasObjects[i].width * canvasObjects[i].scaleX) / 2),
+				otherObjTop = Math.round(canvasObjects[i].top - (canvasObjects[i].height * canvasObjects[i].scaleY) / 2),
+				otherObjRight = Math.round(canvasObjects[i].left + (canvasObjects[i].width * canvasObjects[i].scaleX) / 2),
+				otherObjBottom = Math.round(canvasObjects[i].top + (canvasObjects[i].height * canvasObjects[i].scaleY) / 2);
+            if (canvasObjects[i] !== obj && canvasObjects[i].name !== 'vline' && canvasObjects[i].name !== 'hline') { /* this LEFT matches with Other LEFT */
                 if (otherObjLeft - 1 === movingObjLeft || otherObjLeft + 1 === movingObjLeft) {
                     showAlginLine(obj, "left", "minus");
                 } /* this RIGHT matches with Other RIGHT */
@@ -109,7 +104,7 @@ define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", 
                 if (otherObjBottom - 1 === movingObjBottom || otherObjBottom + 1 === movingObjBottom) {
                     showAlginLine(obj, "top", "plus");
                 } /* this LEFT matches with Other RIGHT */
-                if (otherObjRight - 1 === movingObjLeft || otherObjRight - 1 === movingObjLeft) {
+                if (otherObjRight - 1 === movingObjLeft || otherObjRight + 1 === movingObjLeft) {
                     showAlginLine(obj, "left", "minus");
                 } /* this RIGHT matches with Other LEFT */
                 if (otherObjLeft - 1 === movingObjRight || otherObjLeft + 1 === movingObjRight) {
@@ -131,13 +126,13 @@ define(["matisse", "matisse.util", "matisse.palettes.properties", "matisse.ui", 
         var hLine = matisse.hLine;
         switch (position) {
         case "left":
-            (operator == "plus") ? vLine.set('left', obj.left + (obj.width * obj.scaleX) / 2) : vLine.set('left', obj.left - (obj.width * obj.scaleX) / 2)
+            (operator === "plus") ? vLine.set('left', obj.left + (obj.width * obj.scaleX) / 2) : vLine.set('left', obj.left - (obj.width * obj.scaleX) / 2);
             break;
         case "top":
-            (operator == "plus") ? hLine.set('top', obj.top + (obj.height * obj.scaleY) / 2) : hLine.set('top', obj.top - (obj.height * obj.scaleY) / 2);
+            (operator === "plus") ? hLine.set('top', obj.top + (obj.height * obj.scaleY) / 2) : hLine.set('top', obj.top - (obj.height * obj.scaleY) / 2);
             break;
         }
         canvas.renderAll();
-        hLine.setCoords()
+        hLine.setCoords();
     }
 });
