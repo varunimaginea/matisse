19875dfbba6a40f2cf6e13a300dbcfe71a33d044
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-02-01 14:38:40 +0530

    Addinnew container object

--- public/javascripts/matisse/matisse.containers.devices.js
+++ public/javascripts/matisse/matisse.containers.devices.js
@@ -7,7 +7,18 @@
 
 require(["matisse", "matisse.main", "matisse.containers", "matisse.palettes.properties", "matisse.util"], function (matisse, main, containers, objproperties, util) {
 	"use strict";
-	
+	containers.registercontainer( 'desktop-1024x768' , {
+			 	displayName: "desktop-1024x768",
+				src: "",
+				width: 1024,
+				height: 768,
+				innerWidth:1024,
+				innerHeight:768,
+				xOffset: 0,
+				yOffset: 0,
+				canvasWidth: 1024,
+				canvasHeight: 768
+			});
 	containers.registercontainer( 'iphone' , {
 			 	displayName: "iPhone",
 				src: "iphone_overlay.png",
