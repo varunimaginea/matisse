e919cce93968d53ba1d6364c9c2b58fba18a7b4e
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-02-01 12:50:26 +0530

    Fixing jsLint errors

--- public/javascripts/matisse/matisse.setup.js
+++ public/javascripts/matisse/matisse.setup.js
@@ -5,22 +5,18 @@
  * About this :Entry Point File, All Dom Ready functions need to be defined here
  */
 
-define( ["matisse", "matisse.fabric", "matisse.comm", "matisse.main", "matisse.containers", "matisse.containers.devices", "matisse.layouts", "matisse.layouts.content", "matisse.palettes", "matisse.palettes.basicshapes", "matisse.palettes.wireframe", "matisse.events", "../javascripts/thirdparty/csspopup.js", "matisse.help"] , function (matisse, mfabric, Comm, main, containers, palettes, layouts) {
+define(["matisse", "matisse.fabric", "matisse.comm", "matisse.main", "matisse.containers", "matisse.containers.devices", "matisse.layouts", "matisse.layouts.content", "matisse.palettes", "matisse.palettes.basicshapes", "matisse.palettes.wireframe", "matisse.events", "../javascripts/thirdparty/csspopup.js", "matisse.help"], function (matisse, mfabric, Comm, main, containers, palettes, layouts) {
     //Dom Ready function
     "use strict";
 	$(function () {
-		var serverURL = 'http://localhost';//change it to server ip or local ip for testing from other machines
-		var comm = new Comm(serverURL);
-		comm.onContainerDraw = function(data) {
+		var serverURL = 'http://localhost',//change it to server ip or local ip for testing from other machines
+			comm = new Comm(serverURL);
+		comm.onContainerDraw = function (data) {
 			matisse.containerName = data;
 			layouts.createLayoutsList();
 			containers.createContainerList();
-			
 		}
-		
 		matisse.comm = comm;
 		matisse.main = main;
-        //call all the functions, that are to be called on document ready here;
-		//main.init()
 	});
 });
