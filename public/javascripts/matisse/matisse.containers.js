900f10c4e5876be18c0b9d8934954cf621e53ad6
BhavaniShankar <bhavanishankar.j@imaginea.com>
2012-02-02 11:30:59 +0530

    Fixing jsLint errors

--- public/javascripts/matisse/matisse.containers.js
+++ public/javascripts/matisse/matisse.containers.js
@@ -6,14 +6,12 @@
  *
  */
 define(["matisse", "matisse.main", "matisse.ui", "matisse.util", "matisse.layouts"], function (matisse, main, ui, util, layouts) {
-   
 	return {
 		containerName: null,
-		currSelection:null,
+		currSelection: null,
 		registercontainer : function (containerName, containerDesc) {
 			matisse.containers[containerName] = containerDesc;
 		},
-		
 		/**
 		 * Create a  palette for each type of palette and add it in toolbar
 		 * @method createPallette
@@ -21,46 +19,39 @@ define(["matisse", "matisse.main", "matisse.ui", "matisse.util", "matisse.layout
 		 */
 		createContainerList: function () {
 			var thisRef = this;
-			if(matisse.containerName != 'empty')  {
+			if (matisse.containerName !== 'empty') {
 				this.containerName = matisse.containerName;
 				this.setContainer(this.containerName, 'old');
 				return;
 			}
 			var html = '<div id="containerlist" style="padding:15px"><p>Select a Device</p>';
-			var containerHolder = "<select id='containers' >"
-			for (var contName in matisse.containers) {
+			var containerHolder = "<select id='containers' >";
+			var contName;
+			for (contName in matisse.containers) {
 				var containerObj = matisse.containers[contName];
 				var container_DisplayName = containerObj.displayName;
-				console.log('contName ='+contName);
-				console.log('container_DisplayName ='+container_DisplayName);
-				containerHolder += '<option value='+contName+ ' id='+container_DisplayName+'>'+container_DisplayName;
-				//var src = '/images/' + containerObj.src;
-				//containerHolder += '<div style="padding:15px" class = "container-holder" id="containerholder_' + container_DisplayName + '">';
-				//containerHolder += '<div class="container_image"><img id="' + container_DisplayName + '" src="' + src + '" width="64" height="64" /></div><div id="container-label">' + container_DisplayName + '</div></div>';
-				
+				containerHolder += '<option value=' + contName + ' id=' + container_DisplayName + '>' + container_DisplayName;
 			}
 			html += containerHolder;
-			onButtonClick = function() {
-				//if(thisRef.containerName)
+			onButtonClick = function () {
 				thisRef.onOkClick();
 			}
 			$(document.getElementById('result')).append(html);
-			var btndiv ="<div style='padding:15px'><br><input type='button' value='Ok' onclick=onButtonClick() /></div>"
+			var btndiv = "<div style='padding:15px'><br><input type='button' value='Ok' onclick=onButtonClick() /></div>"
 			$(document.getElementById('result')).append(btndiv);
 			popup('popUpDiv', 'closediv', 300, 600);
 			$('#closediv').css('display', 'none');
 		},
-		onOkClick: function() {
+		onOkClick: function () {
 			this.containerName = document.getElementById('containers').value;
-			console.log('...'+this.containerName);
 			this.setContainer(this.containerName, 'new');
 			matisse.comm.sendContainerInfo({
-					action: "setContainer",
-					containerName:this.containerName
+				action: "setContainer",
+				containerName:this.containerName
 			});
 			$('#closediv').css('display', 'block');
 		},
-		setContainer: function(containerName, type) {
+		setContainer: function (containerName, type) {
 			var contObj = matisse.containers[containerName];
 			ui.deviceHeight = contObj.height;
 			ui.deviceWidth = contObj.width;
@@ -71,20 +62,20 @@ define(["matisse", "matisse.main", "matisse.ui", "matisse.util", "matisse.layout
 			matisse.xOffset = contObj.xOffset;
 			matisse.yOffset = contObj.yOffset;
 			matisse.main.init();
-			var imagsrc = '/images/'+contObj.src;
+			var imagsrc = '/images/' + contObj.src;
 			$('#containerBody').css('background-image', 'url(' + imagsrc + ')');
 			$('#containerBody').css('position', 'relative');
-				var cssObj = {
-							  'position': 'relative',
-							  'left' : contObj.xOffset,
-							  'top' : contObj.yOffset
-							}
+			var cssObj = {
+				'position': 'relative',
+				'left' : contObj.xOffset,
+				'top' : contObj.yOffset
+			};
 			$('#canvasId').css(cssObj);
 			$('.canvas-container').css('width', contObj.canvasWidth);
 			$('.canvas-container').css('height', contObj.canvasHeight);
 			matisse.deviceOffsetX = contObj.xOffset;
 			matisse.deviceOffsetY = contObj.yOffset;
-			if(type == 'new') {
+			if (type === 'new') {
 				closePopup('popUpDiv');
 				closePopup('blanket');
 				var val = document.getElementById('layouts').value;
