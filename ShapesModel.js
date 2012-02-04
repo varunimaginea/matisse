313336c47534c7e71b272173ca5443b0192af37b
Bhavanishankar <bhavanishankar.j@imaginea.com>
2012-02-04 07:39:08 +0530

    Fixing JSLint errors

--- ShapesModel.js
+++ ShapesModel.js
@@ -1,4 +1,4 @@
-var nohm = require(__dirname+'/node_modules/nohm/lib/nohm.js').Nohm;
+var nohm = require(__dirname + '/node_modules/nohm/lib/nohm.js').Nohm;
 
 /**
  * Model definition of a Shapes
@@ -6,98 +6,82 @@ var nohm = require(__dirname+'/node_modules/nohm/lib/nohm.js').Nohm;
 var shapesModel = module.exports = nohm.model('Shapes', {
     idGenerator: 'increment',
     properties: {
-	board_url: {
-	    type: 'string',
-	    index: true,
-	    validations: [
-		'notEmpty'
-	    ]
+		board_url: {
+			type: 'string',
+			index: true,
+			validations: [
+				'notEmpty'
+			]
+		},
+		shapeId: {
+			type: 'string',
+			index: true,
+			validations: [
+				'notEmpty'
+			]
+		},
+		palette: {
+			type: 'string',
+			index: true,
+			validations: [
+				'notEmpty'
+			]
+		},
+		action: {
+			type: 'string',
+			index: true,
+			validations: [
+				'notEmpty'
+			]
+		},
+		args: {
+			type: 'json',
+			validations: [
+				'notEmpty'
+			]
+		}
 	},
-	shapeId: {
-	    type: 'string',
-	    index: true,
-	    validations: [
-		'notEmpty'
-	    ]
-	},
-	palette: {
-	    type: 'string',
-	    index: true,
-	    validations: [
-		'notEmpty'
-	    ]
-	},
-	action: {
-	    type: 'string',
-	    index: true,
-	    validations: [
-		'notEmpty'
-	    ]
-	},
-	args: {
-	    type: 'json',
-	    validations: [
-		'notEmpty'
-	    ]
-	}
-    },
-    methods: {
-	// custom methods we define here to make handling this model easier.
-      
-	/**
-	 * You can specify a data array that might come from the user and an array containing the fields that should be used from used from the data.
-	 * Optionally you can specify a function that gets called on every field/data pair to do a dynamic check if the data should be included.
-	 * The principle of this might make it into core nohm at some point.
-	 */
-	fill: function (data, fields) {
-	    var props = {},
-            self = this;
-	    
-	    fields = Array.isArray(fields) ? fields : Object.keys(data);
-	  
-	    fields.forEach(function (i) {
-		props[i] = data[i];
-	    });
-	    this.p(props);
-	    return props;
-	},
-	
-	/**
-	 * This is a wrapper around fill and save.
-	 */
-	store: function (data, callback) {
-	    var self = this;
+	methods: {
+		// custom methods we define here to make handling this model easier.
+		  
+		/**
+		 * You can specify a data array that might come from the user and an array containing the fields that should be used from used from the data.
+		 * Optionally you can specify a function that gets called on every field/data pair to do a dynamic check if the data should be included.
+		 * The principle of this might make it into core nohm at some point.
+		 */
+		fill: function (data, fields) {
+			var props = {},
+				self = this;
+			fields = Array.isArray(fields) ? fields : Object.keys(data);
+			fields.forEach(function (i) {
+				props[i] = data[i];
+			});
+			this.p(props);
+			return props;
+		},
+		/**
+		 * This is a wrapper around fill and save.
+		 */
+		store: function (data, callback) {
+			var self = this;
+			this.fill(data);
+			this.save(function (err) {
+				if ( ! err) {
+					this.errors; // the errors in validation
+				} else {
+				// it's in the db :)
+					console.log(":::COuld not save to DB:::: " + err);
+				}
+			});	
+		},
 		
-	console.log("===================");
-	//console.log(data.url);
-	//console.log(data.shapeId);
-	console.log(data);
-	console.log("===================");
-	
-	    this.fill(data);
-	    this.save(function (err) {
-		  if ( ! err) {
-			this.errors; // the errors in validation
-			 console.log(":::it's in the db :)::::");
-		  } else {
-			// it's in the db :)
-			 console.log(":::COuld not save to DB:::: "+err);
-		  }
-		});	
-	},
-	
-	/**
-	 * This is a wrapper around fill and save.
-	 */
-	delete: function (data, callback) {
-	    var self = this;
-		
-		console.log("===================");
-		console.log(data);
-		console.log("===================");
-	
-	    this.fill(data);
-	    this.remove();	
-	},
+		/**
+		 * This is a wrapper around fill and save.
+		 */
+		delete: function (data, callback) {
+			var self = this;
+			this.fill(data);
+			this.remove();	
+		},
     }
 });
