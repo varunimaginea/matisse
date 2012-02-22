04e6e3f00c634432b03665bdab6a3b0a0df9786a
Divya <divya.p@imaginea.com>
2012-02-22 15:39:43 +0530

    added table wireframe object

--- public/javascripts/matisse/matisse.palettes.wireframe.js
+++ public/javascripts/matisse/matisse.palettes.wireframe.js
@@ -219,6 +219,143 @@ define(["matisse", "matisse.palettes", "matisse.util", "matisse.palettes.propert
 			}
 		};		
 	};
+	
+	/** Function to obtain text from table object on canvas
+	 * @param obj: table object on canvas from which text is to be extracted.
+	 */
+	var getTableText = function (obj) {
+		var textItems = "", count = 0;
+		if (obj.paths && obj.paths.length >= 3) {
+			textItems = "";
+			// obtain the number of columns in table
+			for (var i = 2; i < obj.paths.length; i++) {
+				if (obj.paths[i].points) {
+					textItems += obj.paths[i - 1].text + "|";
+					count++;
+				}
+			}
+			textItems = textItems.substring(0, textItems.length - 1) + "\n";		
+			var k = 0;
+			// each cell's text is appended with '\|' character
+			for (var j = 2 + (count * 2); j < obj.paths.length; j++) {
+				if (k == count) {
+					// each row is appended with '\n' character
+					textItems = textItems.substring(0, textItems.length - 1) + "\n";
+					k = 0;
+				}
+				if (obj.paths[j].text) {
+					textItems += obj.paths[j].text;
+					k++;
+					textItems = k >= 1 ? textItems + "|": textItems;
+				}				
+			}
+			textItems = textItems.substring(0, textItems.length - 1) + "\n";
+		}
+		return textItems;
+	};
+	/** Function to add table items to the table container on the canvas.
+	 * @param obj: table object on canvas for which the items are to be added.
+	 * @param items: table items (string of table items' text separated by \| and \n char), to be added to the table.
+	 */
+	var addItemsToTable = function (obj, items) {
+		var tableRows = items.split("\n");
+		var objects = [];
+		// store the table container object.
+		objects.push(obj.paths[0]);
+		objects.push(obj.paths[1]);
+		// remove the original table object from the canvas
+		canvas.remove(obj);
+		var tableHeader = tableRows[0],
+			colHeader = tableHeader.split("|"),
+			j = 0;
+		
+		// create table header
+		for (var i = 0; i < colHeader.length; i++) {
+			if (colHeader[i] != '') {
+				var txtObj = new fabric.Text(colHeader[i], {
+					fontSize : 16,
+					fontFamily : "delicious_500",
+					fontWeight : 16,
+					left: -obj.paths[0].width/2 + (obj.paths[0].width/(4 * colHeader.length)) + j++ *(obj.paths[0].width/colHeader.length) + 10,
+					top : -obj.paths[0].height/2 + 10,
+					stroke: '#000000'
+				});			
+				var col = new fabric.Polyline([{x: -obj.paths[0].width/2 + (j *(obj.width / colHeader.length)), y: -obj.height/2}, {x: -obj.paths[0].width/2 + (j *(obj.width / colHeader.length)) + 0.005, y: obj.height/2}],
+							{fill:'#ffffff',stroke:'#aaa'});				
+				objects.push(txtObj);
+				objects.push(col);
+			}
+		}
+		// create table body
+		for (var i = 1; i < tableRows.length; i++) {
+			if (tableRows[i] != '') {
+				var rowElements = tableRows[i].split("|");
+				var txt = "";
+				for (var j=0; j < colHeader.length; j++) {
+					txt = rowElements[j];
+					
+					if (!txt) {
+						txt= " ";
+					}
+					var txtObj = new fabric.Text(txt, {
+						fontSize : 16,
+						fontFamily : "delicious_500",
+						fontWeight : 16,
+						left: -obj.paths[0].width/2 + (obj.paths[0].width/(4 * colHeader.length)) + j *(obj.paths[0].width/colHeader.length) + 10,
+						top : -obj.paths[0].height/2 + (20 * i) + 10,
+						stroke: '#000000'
+					});
+					objects.push(txtObj);					
+				}
+			}
+		}
+		
+		// create a pathgroup for all the above created objects and then add it to the canvas having uid same as that of the original list object.
+		var pathGroup = new fabric.PathGroup(objects, {width: objects[0].width, height: objects[0].height});
+		pathGroup.set({
+			left: obj.left,
+			top: obj.top,
+			angle: 0,
+			scaleX: 1,
+			scaleY: 1
+        });
+		pathGroup.setCoords();
+		pathGroup.name = "table";
+		pathGroup.uid = obj.uid;
+		pathGroup.palette = "wireframe";
+		canvas.add(pathGroup);
+		// render all the items on the canvas after the modification.
+		canvas.renderAll();
+		return pathGroup;
+	}
+	
+	/** Function to provide text area for adding table items and on blur of text area to send the list of table items to get added to the table container.
+	 * @param obj: table object on canvas for which the items are to be added.
+	 */
+	var tableHandler = function (obj) {
+		$("#proptable").append("<tr id = 'txtrow'><td id= 'txttd' valign='top'><label style = 'text-align:right; vertical-align:top' id='labl' for='txtarea'>text:</label></td><td><textarea id='txtarea' cols= '10' style='height:75px'></textarea> </td></tr>");		
+		var txtbox = document.getElementById('txtarea'),
+			count = 0;			
+		txtbox.value = "column1|column2\ncontent1|content2\n";
+
+		// obtain the text from the table, if any.
+		var txt = getTableText(obj);
+		txtbox.value = txt == "" ? txtbox.value : txt;
+		// on blur of txtbox, update the table on canvas with the text in txtbox.
+		txtbox.onblur = function (e) {			
+			if (canvas.getActiveObject()) {
+				var pathGroup = addItemsToTable(canvas.getActiveObject(), txtbox.value);			
+				matisse.comm.sendDrawMsg({
+					action: "modified",
+					args: [{
+						uid: pathGroup.uid,
+						object: pathGroup
+					}]
+				});	
+				canvas.setActiveObject(pathGroup);
+			}
+		};		
+	};
 	/**
 	 * To register wireframe palette
 	 */
@@ -1115,6 +1252,134 @@ define(["matisse", "matisse.palettes", "matisse.util", "matisse.palettes.propert
 				}]	//End of properties for list
 			},
 
+			// Wireframe object, Table has a border, header and columns separated by lines. Text in table cells can be edited in properties dialog.
+			table: {
+				displayName: "table",
+				activeIcon: "table_w.png",
+				inactiveIcon: "table_g.png",
+				// to create a table object
+				toolAction: function (args) {					
+					var objects = [],
+						textItems = "",
+						count = 0,
+						// create a table container initially for the list.. i.e., outer rectangle
+						border = new fabric.Rect({
+							width: args.paths ? args.paths[0].width : 180,
+							height: args.paths ? args.paths[0].height : 200,						
+							fill: '#fdfdfd',
+							stroke: '#000000'
+						}),
+						// add header to the table
+						header = new fabric.Polygon(      
+							[{x: -border.width/2+1, y: -border.height/2+1 },{x: border.width/2 -1 , y: -border.height/2+1},{x: border.width/2 - 1 , y: -border.height/2 + 22},{x: -border.width/2+1, y: -border.height/2 + 22}],
+							{
+								fill: '#efefef',
+								stroke:'#dfdfdf'
+							}
+						);						
+					args.width = border.width;
+					args.height = border.height;
+					args.scaleX = 1;
+					args.scaleY = 1;
+					objects.push(border);
+					objects.push(header);
+					var obj = loadWireframe(args, objects);					
+					// obtain the text of table items from the object, if any.
+					textItems = getTableText(args);
+					// after creating the container, add table items to the container, if any.
+					addItemsToTable(obj, textItems, args);
+				},
+
+				// when list object in one client is modified, modify the same in the other clients connected.
+				modifyAction: function (args) {
+					var obj = util.getObjectById(args.uid);
+					var recvdObj = args.object;
+					var textItems = "",
+						count = 0;
+					updateProperties(obj, recvdObj);
+
+					// update the properties of list container with that of the modified list container.
+					for (var prop in recvdObj.paths[0]) {						
+						obj.paths[0][prop] = recvdObj.paths[0][prop];
+					}
+					obj.paths[1].points = [{x: -recvdObj.width/2+1, y: -recvdObj.height/2+1 },{x: recvdObj.width/2 -1 , y: -recvdObj.height/2+1},{x: recvdObj.width/2 - 1 , y: -recvdObj.height/2 + 22},{x: -recvdObj.width/2+1, y: -recvdObj.height/2 + 22}];
+					// obtain the text of table items from the object, if any.
+					textItems = getTableText(recvdObj);
+					// reconstruct the list with the obtained list items
+					addItemsToTable(obj, textItems);
+				},
+
+				// when table object in one client is resized either width wise or height wise, resize the same in the other clients connected.
+				// change the width or height accordingly and also modify the left and top of the contents 
+				resizeAction: function (resizedObj) {
+					var obj = util.getObjectById(resizedObj.uid), k=1, count=0, j = 0;
+					obj.paths[0].width = resizedObj.width;
+					obj.paths[0].height = resizedObj.height;
+					obj.paths[1].points = [{x: -resizedObj.width/2+1, y: -resizedObj.height/2+1 },{x: resizedObj.width/2 -1 , y: -resizedObj.height/2+1},{x: resizedObj.width/2 - 1 , y: -resizedObj.height/2 + 22},{x: -resizedObj.width/2+1, y: -resizedObj.height/2 + 22}];
+					
+					// If the table has rows and cols
+					if (resizedObj.paths && resizedObj.paths.length >= 3) {
+						// get number of columns in the table
+						for (var i = 2; i < resizedObj.paths.length; i++) {
+							if (resizedObj.paths[i].points) {								
+								count++;
+							}
+						}
+						// for the obtained columns, adjust the height accordingly when resized.
+						for (var i = 2; i < resizedObj.paths.length; i++) {
+							if (resizedObj.paths[i].points) {								
+								obj.paths[i].points = [{x: -resizedObj.paths[0].width/2 + (k *(resizedObj.width / count)), y: -resizedObj.height/2}, {x: -resizedObj.paths[0].width/2 + (k *(resizedObj.width / count)) + 0.005, y: resizedObj.height/2}];
+								k++;
+							}
+						}
+						k = 0;
+						// for the text items, adjust the left and top accordingly when resized either width wise or height wise
+						for (var i = 2; i < resizedObj.paths.length; i++) {
+							if (resizedObj.paths[i].text) {
+								if (k == count) {
+									k = 0;
+									j++;
+								}
+								obj.paths[i].left = -resizedObj.width/2 + (resizedObj.width / count)/4 + (resizedObj.width / count)*k + 10;
+								obj.paths[i].top = -resizedObj.height/2 + (20 * j) + 10; // 20 is the height of each row
+								k++;								
+							}
+						}
+					}
+				},
+
+				// apply the properties of the list object to the properties panel.
+				applyProperties: function (props) {
+					objproperties._applyProperties(props);
+					var activeObj = canvas.getActiveObject();
+					// handle the table object by providing text area to add table items.
+					tableHandler(activeObj);
+				},
+
+				// properties for list object which can be dynamically modified (left, top and angle).
+				properties: [{
+					name: 'left',
+					type: 'number',
+					action: function (args) {
+						(args.obj).set("left", args.property);
+					},
+					defaultvalue: 100
+				}, {
+					name: 'top',
+					type: 'number',
+					action: function (args) {
+						(args.obj).set("top", args.property);
+					},
+					defaultvalue: 100
+				}, {
+					name: 'angle',
+					type: 'number',
+					action: function (args) {
+						(args.obj).set("angle", args.property);
+					},
+					defaultvalue: 0
+				}]	//End of properties for list
+			},
 			slider: {	//Slider wireframe object
 				displayName: "slider",
 				activeIcon: "slider_w.png",
