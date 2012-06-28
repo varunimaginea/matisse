20b5d2e752863ab1086f0d6cc1cd074341bfb7a3
manojiitkgp09 <manoj.iitian@gmail.com>
2012-06-28 17:55:26 +0530

    Issue 125 Add undo for movement of objects

--- public/javascripts/matisse/matisse.events.js
+++ public/javascripts/matisse/matisse.events.js
@@ -6,7 +6,7 @@
  *
  */
 
-define(["matisse", "matisse.ui", "matisse.comm"], function (matisse, ui, comm) {
+define(["matisse", "matisse.ui", "matisse.comm", "matisse.palettes.properties"], function (matisse, ui, comm, properties) {
 	"use strict";
 	return {
 		/**
@@ -15,129 +15,153 @@ define(["matisse", "matisse.ui", "matisse.comm"], function (matisse, ui, comm) {
 		 * @param e keyevent
 		 */
 		keyDown: function (e) {
-            var evt = (e) ? e : (window.event) ? window.event : null;
-            if (evt) {
-                var key = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode : ((evt.which) ? evt.which : 0));
-                if (key == "46" && evt.altKey) { // ALT + DELETE
+      var evt = (e) ? e : (window.event) ? window.event : null;
+      if (evt) {
+        var key = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode : ((evt.which) ? evt.which : 0));
+        if (key == "46" && evt.altKey) { // ALT + DELETE
+            matisse.main.deleteObjects();
+        } else if (key == "38" && evt.ctrlKey) { // CONTROL + Up Arrow
+            var obj = canvas.getActiveObject();
+            if (obj) {
+              canvas.bringForward(obj);
+              notifyZindexChange(obj, 'forward');
+            }
+        } else if (key == "40" && evt.ctrlKey) { // CONTROL + Down Arrow
+            var obj = canvas.getActiveObject();
+            if (obj) {
+                canvas.sendBackwards(obj);
+                notifyZindexChange(obj, 'backward');
+            }
+        } else if (key == "90" && evt.ctrlKey) {
+            var obj = matisse.undoStack.pop();
+            if (typeof(obj) != "undefined") {
+              if (obj.action == "modified") {
+                canvas.getObjects().forEach(function(item, index) {
+                if (item.uid == obj.args[0].uid)
+                  {
+                    matisse.redoStack.push({action: "modified",
+                      name: obj.name,
+                      palette: obj.palette,
+                      path: obj.path,
+                      args: [{
+                        uid: obj.uid,
+                        object: item
+                      }]
+                    });
+                    matisse.comm.sendDrawMsg({
+                      action: obj.action,
+                      name: obj.name,
+                      palette: obj.palette,
+                      path: obj.path,
+                      args: obj.args
+
+                    });
+                      properties.updatePropertyPanel(obj.args[0].object);
+                      matisse.main.modifyObject(obj.args);
+                  //   canvas.setActiveObject(item);
+                  //   matisse.main.deleteObjects();
+                   }
+                });
+              }
+            else if (obj.action == "zindexchange") {
+
+            }
+            else {
+              canvas.getObjects().forEach(function(item, index) {
+                if (item.uid == obj.args[0].uid)
+                  {
+                    matisse.redoStack.push(obj);
+                    canvas.setActiveObject(item);
                     matisse.main.deleteObjects();
-                } else if (key == "38" && evt.ctrlKey) { // CONTROL + Up Arrow
-                    var obj = canvas.getActiveObject();
-                    if (obj) {
-                        canvas.bringForward(obj);
-						notifyZindexChange(obj, 'forward');
-                    }
-                } else if (key == "40" && evt.ctrlKey) { // CONTROL + Down Arrow
-                    var obj = canvas.getActiveObject();
-                    if (obj) {
-                        canvas.sendBackwards(obj);
-						notifyZindexChange(obj, 'backward');
-                    }
-                } else if (key == "90" && evt.ctrlKey) {
-                  var obj = matisse.undoStack.pop();
-                  if (typeof(obj) != "undefined") {
-                    if (obj.action == "modified") {
-
-                    }
-                    else if (obj.action == "zindexchange") {
-
-                    }
-                    else {
-                      canvas.getObjects().forEach(function(item, index) {
-                        if (item.uid == obj.args[0].uid)
-                          {
-                            matisse.redoStack.push(obj);
-                            canvas.setActiveObject(item);
-                            matisse.main.deleteObjects();
-                          }
-                      });
-                    }
                   }
-                }
-                 else if (key == "27") { // when Escape key pressed
-                    closePopup()
-                } else if (key == "37" && evt.shiftKey) {
-                    var obj = canvas.getActiveObject();
-                    var objleft = obj.left;
-                    obj.set('left', objleft - (matisse.horIndent * matisse.indentMultiplier));
-                    onObjectMoveByKey(obj);
-                } else if (key == "37") {
-                    var obj = canvas.getActiveObject();
-                    var objleft = obj.left;
-                    obj.set('left', objleft - matisse.horIndent);
-                    onObjectMoveByKey(obj)
-                } else if (key == "39" && evt.shiftKey) {
-                    var obj = canvas.getActiveObject();
-                    var objleft = obj.left;
-                    obj.set('left', objleft + (matisse.horIndent * matisse.indentMultiplier));
-                    onObjectMoveByKey(obj);
-                } else if (key == "39") {
-                    var obj = canvas.getActiveObject();
-                    var objleft = obj.left;
-                    obj.set('left', objleft + matisse.horIndent);
-                    onObjectMoveByKey(obj)
-                } else if (key == "38" && evt.shiftKey) {
-                    var obj = canvas.getActiveObject();
-                    var objtop = obj.top;
-                    obj.set('top', objtop - matisse.verIndent * matisse.indentMultiplier);
-                    onObjectMoveByKey(obj)
-                } else if (key == "38") {
-                    var obj = canvas.getActiveObject();
-                    var objtop = obj.top;
-                    obj.set('top', objtop - matisse.verIndent);
-                    onObjectMoveByKey(obj)
-                } else if (key == "40" && evt.shiftKey) {
-                    var obj = canvas.getActiveObject();
-                    var objtop = obj.top;
-                    obj.set('top', objtop + matisse.verIndent * matisse.indentMultiplier);
-                    onObjectMoveByKey(obj)
-                } else if (key == "40") {
-                    var obj = canvas.getActiveObject();
-                    var objtop = obj.top;
-                    obj.set('top', objtop + matisse.verIndent);
-                    onObjectMoveByKey(obj)
-                }
+              });
             }
-        },
+          }
+        }
+         else if (key == "27") { // when Escape key pressed
+            closePopup()
+        } else if (key == "37" && evt.shiftKey) {
+            var obj = canvas.getActiveObject();
+            var objleft = obj.left;
+            obj.set('left', objleft - (matisse.horIndent * matisse.indentMultiplier));
+            onObjectMoveByKey(obj);
+        } else if (key == "37") {
+            var obj = canvas.getActiveObject();
+            var objleft = obj.left;
+            obj.set('left', objleft - matisse.horIndent);
+            onObjectMoveByKey(obj)
+        } else if (key == "39" && evt.shiftKey) {
+            var obj = canvas.getActiveObject();
+            var objleft = obj.left;
+            obj.set('left', objleft + (matisse.horIndent * matisse.indentMultiplier));
+            onObjectMoveByKey(obj);
+        } else if (key == "39") {
+            var obj = canvas.getActiveObject();
+            var objleft = obj.left;
+            obj.set('left', objleft + matisse.horIndent);
+            onObjectMoveByKey(obj)
+        } else if (key == "38" && evt.shiftKey) {
+            var obj = canvas.getActiveObject();
+            var objtop = obj.top;
+            obj.set('top', objtop - matisse.verIndent * matisse.indentMultiplier);
+            onObjectMoveByKey(obj)
+        } else if (key == "38") {
+            var obj = canvas.getActiveObject();
+            var objtop = obj.top;
+            obj.set('top', objtop - matisse.verIndent);
+            onObjectMoveByKey(obj)
+        } else if (key == "40" && evt.shiftKey) {
+            var obj = canvas.getActiveObject();
+            var objtop = obj.top;
+            obj.set('top', objtop + matisse.verIndent * matisse.indentMultiplier);
+            onObjectMoveByKey(obj)
+        } else if (key == "40") {
+            var obj = canvas.getActiveObject();
+            var objtop = obj.top;
+            obj.set('top', objtop + matisse.verIndent);
+            onObjectMoveByKey(obj)
+        }
+      }
+    },
 
-        /**
-         * Listen for mouse down(on canvas after shape tool is selected) event and do necessary action
-         * @method mouseDown 
-         * @param e mouseevent
-         */
-        mouseDown: function (event) {
-			if (document.getElementById('delete_menuItem') && event.button == 0) { //if it is left click, remove the context menu item, if any
-				$('#delete_menuItem').remove();
-			}
-            if (!canvas.isDrawingMode && matisse.drawShape) {
-				matisse.points.x = event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset; //offset
-                matisse.points.y = event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset; //offset
-                matisse.shapeArgs[0].left = matisse.points.x;
-                matisse.shapeArgs[0].top = matisse.points.y;
-                matisse.shapeArgs[0].name = matisse.action;
-                matisse.shapeArgs[0].palette = matisse.paletteName;
-                matisse.palette[matisse.paletteName].shapes[matisse.action].toolAction.apply(this, matisse.shapeArgs);
-                matisse.comm.sendDrawMsg({
-                    palette: matisse.paletteName,
-                    action: matisse.action,
-                    args: matisse.shapeArgs
-                });
-                canvas.isSelectMode = true;
-                matisse.drawShape = false;
-                ui.resetShapeSelection();
-                matisse.undoStack.push({
-                    name: matisse.action,
-                    palette: matisse.paletteName,
-                    action: matisse.action,
-                    args: matisse.shapeArgs
-                });
-            }
-            if (canvas.isDrawingMode) {
-                matisse.xPoints = [];
-                matisse.yPoints = [];
-                matisse.xPoints.push(event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset);
-                matisse.yPoints.push(event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset);
-            }
-        },
+    /**
+     * Listen for mouse down(on canvas after shape tool is selected) event and do necessary action
+     * @method mouseDown
+     * @param e mouseevent
+     */
+    mouseDown: function (event) {
+      if (document.getElementById('delete_menuItem') && event.button == 0) { //if it is left click, remove the context menu item, if any
+        $('#delete_menuItem').remove();
+      }
+      if (!canvas.isDrawingMode && matisse.drawShape) {
+        matisse.points.x = event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset; //offset
+        matisse.points.y = event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset; //offset
+        matisse.shapeArgs[0].left = matisse.points.x;
+        matisse.shapeArgs[0].top = matisse.points.y;
+        matisse.shapeArgs[0].name = matisse.action;
+        matisse.shapeArgs[0].palette = matisse.paletteName;
+        matisse.palette[matisse.paletteName].shapes[matisse.action].toolAction.apply(this, matisse.shapeArgs);
+        matisse.comm.sendDrawMsg({
+            palette: matisse.paletteName,
+            action: matisse.action,
+            args: matisse.shapeArgs
+        });
+        canvas.isSelectMode = true;
+        matisse.drawShape = false;
+        ui.resetShapeSelection();
+        matisse.undoStack.push({
+            palette: matisse.paletteName,
+            action: matisse.action,
+            args: matisse.shapeArgs
+        });
+      }
+      if (canvas.isDrawingMode) {
+          matisse.xPoints = [];
+          matisse.yPoints = [];
+          matisse.xPoints.push(event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset);
+          matisse.yPoints.push(event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset);
+      }
+    },
 
 		// Listen for right click of mouse and display context menu when any object on canvas is selected.
 		contextMenu: function (event) {
@@ -175,73 +199,74 @@ define(["matisse", "matisse.ui", "matisse.comm"], function (matisse, ui, comm) {
 				}
 			}
 		},
-        /**
-         * Listen for mouse move event and do necessary action
-         * @method mouseMove 
-         * @param e mouseevent
-         */
-        mouseMove: function (event) {			
-			matisse.eventObj = event;
-            if (canvas.isDrawingMode) {
-                matisse.xPoints.push(event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset);
-                matisse.yPoints.push(event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset);
-            }
-        },
+    /**
+     * Listen for mouse move event and do necessary action
+     * @method mouseMove
+     * @param e mouseevent
+     */
+    mouseMove: function (event) {
+      matisse.eventObj = event;
+      if (canvas.isDrawingMode) {
+          matisse.xPoints.push(event.pageX + document.getElementById("canvasId").scrollLeft + document.getElementById("containerDiv").scrollLeft - matisse.xOffset);
+          matisse.yPoints.push(event.pageY + document.getElementById("canvasId").scrollTop + document.getElementById("containerDiv").scrollTop - matisse.yOffset);
+      }
+    },
 
-        /**
-         *  Notify Server about Group Moved
-         *  @method  notifyServerGroupMoved
-         *  @param none
-         */
-        notifyServerGroupMoved: function () {
-            var activeGroup = canvas.getActiveGroup();
-            var objectsInGroup = activeGroup.getObjects();
-            canvas.discardActiveGroup();
-            objectsInGroup.forEach(function (obj) {
-                notifyObjModify(obj);
-            });
-        }
+    /**
+     *  Notify Server about Group Moved
+     *  @method  notifyServerGroupMoved
+     *  @param none
+     */
+    notifyServerGroupMoved: function () {
+      var activeGroup = canvas.getActiveGroup();
+      var objectsInGroup = activeGroup.getObjects();
+      canvas.discardActiveGroup();
+      objectsInGroup.forEach(function (obj) {
+          notifyObjModify(obj);
+      });
+    }
     };
     return eve;
 
     function closePopup() {
-        var popEl = document.getElementById('popUpDiv');
-        var closeEl = document.getElementById('closediv');
-        var blanketEl = document.getElementById('blanket');
-        if (popEl.style.display != 'none') popEl.style.display = 'none';
-        if (closeEl.style.display != 'none') closeEl.style.display = 'none';
-        if (blanketEl.style.display != 'none') blanketEl.style.display = 'none'
+      var popEl = document.getElementById('popUpDiv');
+      var closeEl = document.getElementById('closediv');
+      var blanketEl = document.getElementById('blanket');
+      if (popEl.style.display != 'none') popEl.style.display = 'none';
+      if (closeEl.style.display != 'none') closeEl.style.display = 'none';
+      if (blanketEl.style.display != 'none') blanketEl.style.display = 'none'
     }
 
     function onObjectMoveByKey(obj) {
-        canvas.renderAll();
-        obj.setCoords();
-        canvas.fire('object:moving', {
-            target: obj
-        });
-		notifyObjModify(obj);
+      canvas.renderAll();
+      obj.setCoords();
+      canvas.fire('object:moving', {
+          target: obj
+      });
+      notifyObjModify(obj);
     }
-	function notifyObjModify(obj) {
-		matisse.comm.sendDrawMsg({
-			action: "modified",
-			name: obj.name,
-			palette: obj.palette,
-			args: [{
-				uid: obj.uid,
-				object: obj
-			}] // When sent only 'object' for some reason object  'uid' is not available to the receiver method.
-		});
-	}
-	function notifyZindexChange(obj, changType) {
-		matisse.comm.sendDrawMsg({
-			action: "zindexchange",
-			name: obj.name,
-			palette: obj.palette,
-			args: [{
-				uid: obj.uid,
-				object: obj,
-				change : changType,
-			}] // When sent only 'object' for some reason object  'uid' is not available to the receiver method.
-		});
-	}
+    function notifyObjModify(obj) {
+      matisse.comm.sendDrawMsg({
+        action: "modified",
+        //name: obj.name,
+        palette: obj.palette,
+        args: [{
+          uid: obj.uid,
+          object: obj
+        }] // When sent only 'object' for some reason object  'uid' is not available to the receiver method.
+
+      });
+    }
+    function notifyZindexChange(obj, changType) {
+      matisse.comm.sendDrawMsg({
+        action: "zindexchange",
+        name: obj.name,
+        palette: obj.palette,
+        args: [{
+          uid: obj.uid,
+          object: obj,
+          change : changType,
+        }] // When sent only 'object' for some reason object  'uid' is not available to the receiver method.
+      });
+    }
 });
