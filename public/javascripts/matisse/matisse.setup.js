aed1647dbffd3579cdb705a780a97a5d18acd811
Krishna Karthik <nkkarthik@gmail.com>
2012-07-19 23:43:59 +0530

    Fixed #161 User should be able to see other users viewing/working on the same board
    Approach: User, upon joining a board, broadcasts an event 'hello' with
    his name to everyone.  Other users on the board broadcast back their
    names to everyone too.  On user disconnect server broadcasts an event
    'bye' to everyone on the board.
    
    Server changes:
    -----------------
    server/collaboration.js:
     - When a user says 'hello' broadcast it.
     - When a user disconnects broadcast a 'bye' event.
     - 'board' and 'name' are set as socket attributes.
    
    Client changes:
    -----------------
    board.html: has new image (bottom right) 'Friends' when clicked pops
    other users' names.
    shared-users.js: listens to 'hello' and 'bye' and updates friends list
    matisse.main.js: friends icon on click show users list
    matisse.setup.js: import and initialize shared-users feature.
    
    Other change: Renamed effects.js to modified-by-user.js and moved it
    to features directory
    
    Drawback:
    -----------------
    Should try to use only one broadcast message instead of everyone
    broadcasting their name to everyone else.

--- public/javascripts/matisse/matisse.setup.js
+++ public/javascripts/matisse/matisse.setup.js
@@ -5,7 +5,7 @@
  * About this :Entry Point File, All Dom Ready functions need to be defined here
  */
 
-define(["matisse", "matisse.fabric", "matisse.comm", "matisse.main", "matisse.containers", "matisse.containers.devices", "matisse.layouts", "matisse.effects", "matisse.layouts.content", "matisse.palettes", "matisse.palettes.basicshapes", "matisse.palettes.wireframe", "matisse.palettes.components", "matisse.events", "../javascripts/thirdparty/csspopup.js", "matisse.help"], function (matisse, mfabric, Comm, main, containers, palettes, layouts, effects) {
+define(["matisse", "matisse.fabric", "matisse.comm", "matisse.main", "matisse.containers", "matisse.containers.devices", "matisse.layouts", "features/modified-by-user", "features/shared-users", "matisse.layouts.content", "matisse.palettes", "matisse.palettes.basicshapes", "matisse.palettes.wireframe", "matisse.palettes.components", "matisse.events", "../javascripts/thirdparty/csspopup.js", "matisse.help"], function (matisse, mfabric, Comm, main, containers, palettes, layouts, modifiedByUser, sharedUsers) {
     
     "use strict";
 	//Dom Ready function
@@ -66,6 +66,7 @@ define(["matisse", "matisse.fabric", "matisse.comm", "matisse.main", "matisse.co
               matisse.main = main;
               main.addTools();
 
-              effects.showModifiedByUser(matisse);
+              modifiedByUser.init();
+              sharedUsers.init();
 	  });
 });
